1bf03da15da5d76f7faf988162e6370b3aa28195
bennitalg <bennitalg@outlook.com>
2018-09-06 13:04:24 -0500

    FullName2 - Why is this not compiling :(

--- CG 6-1/Program.cs
+++ CG 6-1/Program.cs
@@ -6,7 +6,29 @@ namespace CG_6_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            /*Console.WriteLine(“What’s In A Name?”);
+            Console.Write(“Enter your first name:  “);
+            string firstName = Console.Readline();
+            Console.Write(“Enter your last name:  “);
+            string lastName = Console.ReadLine();
+            */
+
+
+            //Declare a StringBuilder
+            System.Text.StringBuilder builder = new System.Text.StringBuilder();
+            builder.Append("What is your first name? ");
+            builder.AppendLine();
+            builder.Append("What is your last name? ");
+            builder.AppendLine();
+
+            // Get a reference to the StringBuilder's buffer content.
+            string innerString = builder.ToString();
+
+            // Display with Debug.
+            System.Diagnostics.Debug.WriteLine(innerString);
+
+            Console.ReadLine();
         }
     }
 }
